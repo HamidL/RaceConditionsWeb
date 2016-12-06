fdb8a0fb2124e146bdf1856d24c00969b92fc922
Miquel Ferriol <mik_ferri@hotmail.com>
2016-12-06 22:22:20 +0100

    table

---
+++ app/table/table.component.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('raceConditions').
+  component('formTable', {
+    templateUrl:'table/table.view.html',
+    controller: function tableController($scope) {
+        $scope.table = {
+            "tableName": "TablapruebaaghzfmhsbW1mZ3IRCxIEVXNlchiAgICA-MKECgw",
+            "columnNames": [
+                "a",
+                "b"
+            ],
+            "columns": [
+                {
+                    "id": "aghzfmhsbW1mZ3I_CxIyVGFibGFwcnVlYmFhZ2h6Zm1oc2JXMW1aM0lSQ3hJRVZYTmxjaGlBZ0lDQS1NS0VDZ3cYgICAgNq3lwkM",
+                    "colmnValues": {
+                        "b": 12,
+                        "a": "12"
+                    }
+                },
+                {
+                    "id": "aghzfmhsbW1mZ3I_CxIyVGFibGFwcnVlYmFhZ2h6Zm1oc2JXMW1aM0lSQ3hJRVZYTmxjaGlBZ0lDQS1NS0VDZ3cYgICAgN6lhQoM",
+                    "colmnValues": {
+                        "b": 5,
+                        "a": "12"
+                    }
+                },
+                {
+                    "id": "aghzfmhsbW1mZ3I_CxIyVGFibGFwcnVlYmFhZ2h6Zm1oc2JXMW1aM0lSQ3hJRVZYTmxjaGlBZ0lDQS1NS0VDZ3cYgICAgN6zlAoM",
+                    "colmnValues": {
+                        "b": 8.05,
+                        "a": "6"
+                    }
+                },
+                {
+                    "id": "aghzfmhsbW1mZ3I_CxIyVGFibGFwcnVlYmFhZ2h6Zm1oc2JXMW1aM0lSQ3hJRVZYTmxjaGlBZ0lDQS1NS0VDZ3cYgICAgN7-mAoM",
+                    "colmnValues": {
+                        "b": 12,
+                        "a": "12"
+                    }
+                }
+            ]
+        }
+    }
+  });
